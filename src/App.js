0ee0928c5fb90406d2ba2832a9f50a2df25b93dc
iradavydova <irinadavydovawork@gmail.com>
2022-05-04 20:44:36 +0200

    different colors of the blocks, bigger block realization

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Tariff from './components/Tariff';
 
 function App() {
   const rates = [
-    { name: "Безлимитный 300", price: "300", speed: "До 10 Мбит/сек"},
-    { name: "Безлимитный 450", price: "450", speed: "До 50 Мбит/сек"},
-    { name: "Безлимитный 550", price: "550", speed: "До 100 Мбит/сек", isBigger:true},
-    { name: "Безлимитный 1000", price: "1000", speed: "До 200 Мбит/сек"},
+    { name: "Безлимитный 300", price: "300", speed: "До 10 Мбит/сек", priceColor: "rgba(44,193,213,255)", nameColor:"rgba(34,156,171,255)"},
+    { name: "Безлимитный 450", price: "450", speed: "До 50 Мбит/сек", priceColor: "rgba(36,174,141,255)", nameColor: "rgba(32,135,114,255)" },
+    { name: "Безлимитный 550", price: "550", speed: "До 100 Мбит/сек", isBigger:true, priceColor: "rgba(244,106,95,255)", nameColor: "rgba(196,85,78,255)"},
+    { name: "Безлимитный 1000", price: "1000", speed: "До 200 Мбит/сек", priceColor: "rgba(51,58,68,255)", nameColor: "rgba(40,47,55,255)"},
    ]
 
   return (
     <div className="App">
       {rates.map((rate) =>
-      <Tariff name={rate.name} price={rate.price} speed={rate.speed} isBigger={rate.isBigger}/>
+      <Tariff name={rate.name} price={rate.price} speed={rate.speed} isBigger={rate.isBigger} priceColor={rate.priceColor} nameColor={rate.nameColor}/>
       )
       }
          
--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -3,12 +3,12 @@ import './tariff.css'
 
 class Tariff extends React.Component {
     render() {
-    const {name, price, speed, isBigger} = this.props;
+    const {name, price, speed, isBigger, priceColor, nameColor} = this.props;
     
     return (
-        <div className={"rate_card"+(isBigger?"rate_card_big":"")}>
-        <div className='rate_name'>{name}</div>
-        <div className='rate_price'><a className='small_font'>руб</a>{price}<a className='small_font'>/мес</a></div>
+        <div className={'rate_card'+(isBigger?' rate_card_big':'')}>
+        <div className='rate_name' style = {{backgroundColor: nameColor}}>{name}</div>
+        <div className='rate_price' style = {{backgroundColor: priceColor}}><a className='small_font'>руб</a>{price}<a className='small_font'>/мес</a></div>
         <div className='rate_speed'>{speed}</div>
         <div className='rate_extra'>Объем включенного трафика не ограничен</div>
     </div>
